c116ffaf17867d19c788fce06724a86413227d60
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-12-22 10:36:41 +0530

    logical operators

--- Operators.py
+++ Operators.py
@@ -9,3 +9,10 @@ print(10>5)
 print(10>=5)
 print(10<=5)
 print(10 != 5)
+
+#logical operators
+print((10>5) and (5>2) and "A" == "c")
+print((10>5) and (5>2) or "A" == "c")
+print((10>5) and (5>2))
+print((10>5) & (5>2))
+print(not("imesha"=="dilshani"))
