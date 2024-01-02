a8551a401663cf8ede3fbe58a33aec40fcb82e57
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-01-02 17:27:58 +0530

    file handling

---
+++ file-handling/open-file.py
@@ -0,0 +1,13 @@
+f = open("C:/Users/Asus/Documents/GitHub/beginner-python-scripts/file-handling/demofile.txt", "r")
+
+print(f.read())
+
+f = open("C:/Users/Asus/Documents/GitHub/beginner-python-scripts/file-handling/demofile.txt", "r")
+print(f.readline())
+
+
+f = open("C:/Users/Asus/Documents/GitHub/beginner-python-scripts/file-handling/demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
+f.close()
