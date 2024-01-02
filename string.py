ba0d878554eaa0dbf01b21c6ea205d80d989de5e
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-01-02 10:33:52 +0530

    multiline string

--- string.py
+++ string.py
@@ -2,3 +2,9 @@ brand="Imesha"
 print(len(brand))
 print(brand != "imesha")
 print("code" not in brand)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
