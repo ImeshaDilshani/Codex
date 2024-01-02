9956aa5b398131ee53d960bb5201e76845154c5d
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-01-02 18:05:50 +0530

    Compound Statements

--- Compound-Statements/conditional.py
+++ Compound-Statements/conditional.py
@@ -4,3 +4,22 @@ if a>b:
     print("a is greater than b")
 else:   
     print("b is greater than a")
+
+#while loop
+i =1
+while i<6:
+    print(i)
+    i+=1
+
+#for loop
+fruits=["apple","banana","cherry"]
+for x in fruits:
+    print(x)
+
+#nested loop
+adj=["red","big","tasty"]
+fruits=["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+        
