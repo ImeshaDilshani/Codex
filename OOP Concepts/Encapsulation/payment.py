160a7dc9168f33543f2f00aaf20729db39c70ebb
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-01-03 22:18:41 +0530

    payment example

--- OOP Concepts/Encapsulation/payment.py
+++ OOP Concepts/Encapsulation/payment.py
@@ -1,8 +1,15 @@
 class Payment:
     def __init__(self, price):
-        self.__final_price = price + price * 0.1
+        self.__final_price = price + price * 0.05
 
-book = Payment(100)
+    def get_final_price(self):
+        return self.__final_price
+        
+    def set_final_price(self, discount):
+        self.__final_price = self.__final_price - (self.__final_price*(discount/100))
+
+book = Payment(10)
 # print(book._final_price)
 # book.__final_price = 200
-print(book.__final_price)
+book.set_final_price(10)
+print(book.get_final_price())
