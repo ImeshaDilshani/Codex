e57013b9a7cee4bfeb5be10b0dcbae8578b7b0bf
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-01-03 18:24:13 +0530

    encapsulation

--- OOP Concepts/encapsulation.py
+++ OOP Concepts/encapsulation.py
@@ -1,7 +1,17 @@
+# class myClass:
+#     x = 5
+#     __y = 10
+
+# myObject = myClass()
+# print(myObject.x)
+# print(myObject._myClass__y) # This is how you access private variables in Python
+
 class myClass:
-    x = 5
-    __y = 10
+    def meth1(self):
+        print("myClass meth1")
+    def __meth2(self):
+        print("myClass meth2")
 
 myObject = myClass()
-print(myObject.x)
-print(myObject._myClass__y) # This is how you access private variables in Python
+myObject.meth1()
+# myObject.__meth2() # This will cause an error
